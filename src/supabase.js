9a58f87316d82a6bfa7deee67aaf66cddd2223ba
307国道牛师傅 <2773540973@qq.com>
2025-11-16 17:31:25 +0800

    添加正确的key

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // 请替换为您的Supabase项目URL和anon key
 const supabaseUrl = 'https://ztembyarrbkwprmzscuy.supabase.co'
-const supabaseAnonKey = process.env.SUPABASE_KEY
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZW1ieWFycmJrd3BybXpzY3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNTExOTIsImV4cCI6MjA3ODgyNzE5Mn0.gcUbRtxOYEK7Zbf4kfaMf4WSPerFTLUC0Z1GRUiG7kI'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
